ad91110936bb188f34f248b612935e40fc436265
descomkraneoz <36744638+descomkraneoz@users.noreply.github.com>
2018-11-13 09:27:07 +0100

    Add files via upload

--- TiempoDeViaje.java
+++ TiempoDeViaje.java
@@ -3,27 +3,32 @@ import javax.swing.*;
 public class TiempoDeViaje {
     private static final int CERO_MINUTOS = 0;
     private static final int SESENTA_MINUTOS = 60;
-    private static final int MINUTO_NUEVE=9;
+    private static final int MINUTO_NUEVE = 9;
 
     public static void main(String[] args) {
 
-        int minutos=0;
+        int minutos = 0;
         int tiempoTramo;
-        int horas=0;
+        int horas = 0;
+        String salida="Tiempo total de viaje: "+horas+":";
 
         do {
-            tiempoTramo= Integer.parseInt(JOptionPane.showInputDialog("Ingresa el tiempo en minutos para el tramo de viaje"));
-            System.out.println("Duración tramo: "+tiempoTramo);
-            minutos=tiempoTramo+minutos;
-        }while (tiempoTramo!=CERO_MINUTOS);
-        if (minutos>=SESENTA_MINUTOS){
+            tiempoTramo = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el tiempo en minutos para el tramo de viaje"));
+            System.out.println("Duración tramo: " + tiempoTramo);
+            minutos = tiempoTramo + minutos;
+        } while (tiempoTramo != CERO_MINUTOS);
+        if (minutos >= SESENTA_MINUTOS) {
             horas++;
-            minutos=minutos%SESENTA_MINUTOS;
+            minutos = minutos % SESENTA_MINUTOS;
         }
-        if (minutos<=MINUTO_NUEVE){
-            System.out.println("Tiempo total de viaje: "+horas+":"+0+minutos+" horas");
-        }else{
-            System.out.println("Tiempo total de viaje: "+horas+":"+minutos+" horas");
+        if (minutos <= MINUTO_NUEVE) {
+            salida+=0;
+            //System.out.println("Tiempo total de viaje: " + horas + ":" + 0 + minutos + " horas");
+        } else {
+            salida+="";
+            //System.out.println("Tiempo total de viaje: " + horas + ":" + minutos + " horas");
         }
+        salida+=minutos+" horas";
+        System.out.println(salida);
     }
 }
